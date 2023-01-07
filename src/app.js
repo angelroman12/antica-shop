40ac1b22e36c041b245e569261d5f3b7ab22f9e0
angelroman12 <romanangel95@yahoo.com>
2023-01-07 16:59:15 +0200

    varsta category

--- src/app.js
+++ src/app.js
@@ -110,6 +110,16 @@ function showCategory() {
 	categoryDiv.classList.toggle('display-none');
 }
 
+const varstaBtn = document.getElementById('varsta-filter-btn');
+const varstaDiv = document.querySelector('.varsta-btn');
+
+varstaBtn.addEventListener('click', showVarsta);
+function showVarsta() {
+	varstaDiv.classList.toggle('display-none')
+}
+
+
+
 const coverBtn = document.getElementById('cover-filter-btn')
 const coverDiv = document.querySelector('.cover-btn')
 
@@ -124,29 +134,3 @@ const year = document.getElementById('year')
 const currentYear = new Date().getFullYear()
 year.innerHTML = currentYear
 
-
-// const myString = 'ken follet'
-// const lowerString = myString.split(' ');
-
-// console.log(lowerString)
-// console.log(lowerString[0].split('')[0].toLocaleUpperCase())
-// console.log(lowerString[1])
-
-function convertString(givenString) {
-	const newString = givenString.split(" ");
-
-
-	const firstName = newString[0].split('');
-	const sliced = firstName.slice(1).join('');
-	const firstBigLetter = firstName[0].toUpperCase();
-	const complateName = firstBigLetter + sliced
-
-	const secondName = newString[1].split('');
-	const secondSlice = secondName.slice(1).join("");
-	const secondBigLetter = secondName[0].toUpperCase();
-	const complateSecondName = secondBigLetter + secondSlice;
-	const fullName = complateName + ' ' + complateSecondName;
-	return fullName;
-} 
-
-// console.log(convertString('ken follet'))
